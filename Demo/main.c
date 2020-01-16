d090bd241415cb983305166a5223f8db0814ec63
Wang Yi <yi.wang@afconsult.com>
2020-01-16 14:32:19 +0800

    test2
    Change-Id: I8004feeccdb6d56278eed99030c16566b3629ebd

--- Demo/main.c
+++ Demo/main.c
@@ -160,6 +160,7 @@ int main( void )
 	of this file. */
 	#if ( mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 1 )
 	{
+		printf( "Uncomment the call to kbhit() in this file to also dump trace with a key press.\r\n" );
 		main_blinky();
 	}
 	#else
